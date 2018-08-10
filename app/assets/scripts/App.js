30f22f6521f13ea4b9ea8b0d5c844411f1faba53
Nikola <nikola8989@hotmail.com>
2018-08-10 22:06:04 +0200

    complete reveal on scroll elements

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
 
-import MobileMenu from "./modules/MobileMenu";
-
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");
+
+
